4b195733ba578c9542c99703c030ef52d6cff1e9
Le Zheng <229636060@qq.com>
2020-06-28 17:42:11 +0800

    zll

--- serve.js
+++ serve.js
@@ -6,23 +6,34 @@ var connection = require("./sql.js");
 var rdata = require("./returndata.js");
 var jwt = require('jsonwebtoken')
 let secretOrPrivateKey="jwt";// 这是加密的key（密钥）
-app.post('*',(req,res_app,next) =>{
+app.use((req,res_app,next) =>{
     var token = req.headers['token'];
-    console.log(token)
+    if(req.path == '/login'){
+        return next()
+    }
     if(token == undefined){
-          return next();
+          res_app.send(rdata(-200,'','还未登录'));
+          return false;
       }else{
           verToken(token).then((data)=> {
               req.data = data;
               return next();
           }).catch((error)=>{
-        　　　　console.log(error);
-              return next();
+            　　　　res_app.send(rdata(-200,'','还未登录'));
+                   return false;
           })
       }
 })
+
 //登录提交
 app.post('/login',(req, res_app) => {
+    console.log("a")
+    console.log(req.query.useid)
+    console.log(req.query.password)
+    if(req.query.useid == '' || req.query.password == ''){
+        res_app.send(rdata(-1,'','请填写用户名或密码'));
+        return false
+    }
     var sql_order = "select * from useinfo WHERE usename = '" + req.query.useid + "' AND `password` = '" + req.query.password+"'"
     connection.query(sql_order,function(err,res,next){
         if(err){
@@ -33,7 +44,6 @@ app.post('/login',(req, res_app) => {
             res_app.send(rdata(-1,JSON.parse(JSON.stringify(res)),'用户名或密码错误'));
         }else{
             let content ={name:req.query.useid}; // 要生成token的主题信息
-            
             let token = jwt.sign(content, secretOrPrivateKey, {
                 expiresIn: 60*60*1  // 1小时过期
             });
